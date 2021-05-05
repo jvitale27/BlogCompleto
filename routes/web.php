31f5301effac919b0a5808218cf3e4fd9968ccbd
Julian Vitale <jvitale27@gmail.com>
2021-05-05 19:36:05 -0300

    Sigo agregando...

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,8 @@ Route::get('/', function () {
 */
 Route::get('/', [PostController::class, 'index'])->name('posts.index');
 
+Route::get('posts/{post}', [PostController::class, 'show'])->name('posts.show');
+
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
